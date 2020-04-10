9aa4fed4157be4b9a377f14d5c4a917f221ba441
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2020-04-10 09:42:22 +0530

    Final Changes

--- FastAdaptiveHuffmanCoding.py
+++ FastAdaptiveHuffmanCoding.py
@@ -4,20 +4,23 @@ Created on Tue Feb 25 22:50:10 2020
 
 @author: Digvijay Singh
 """
+import time
+start_time = time.time()
+
 from Front_Tree import Add_Front as AF
 from Front_Tree import Remove_Front as RF
 from Back_Tree import Code_Back as CB
 from Back_Tree import Insert_Back as IB
 from Back_Tree import Remove_Back as RB
 
-Input = "ACECEF"
+Input = "ACECEFJHDBEH"
 
 Special_Codeword = '$'
 Time_Constant = 4
 
 W = Special_Codeword
 T = Time_Constant
-Alphabet_Range = 8
+Alphabet_Range = 26
 
 FT = { }        ## Front Tree, d1
 FTC = { }        ## Front Tree, d2
@@ -36,6 +39,7 @@ for i in range(Alphabet_Range):
     char = chr(num)
 OS = []         ## Output Symbols
 OC = []         ## Output Code
+Newly_Added = []
 count = 0
 for ch in Input:
     if (ch in BT):
@@ -49,15 +53,28 @@ for ch in Input:
         AF(ch, FT, FTC)
         OC.append(code)
         OS.append(ch)
+        Newly_Added.append(1)
     else:
         code = FTC[ch]
         OS.append(ch)
         OC.append(code)
         AF(ch, FT, FTC)
-    print(OS)
-    print(OC)
+        Newly_Added.append(0)
+   
     if(count >= T):
+        if Newly_Added[count-T] == 1:
+            RF(W, FT, FTC)
         RF(Input[count-T], FT, FTC)
         IB(Input[count-T], BT)
         
     count = count + 1
+
+
+#print(OS)
+#print(OC)
+bit_length = 0
+for code in OC:
+    bit_length += len(code)
+CR1 = (bit_length)/len(Input)*100/8
+print("Compression ratio is = ", CR1)    
+print("--- %s seconds ---" % (time.time() - start_time))
